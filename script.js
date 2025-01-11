cce3b7c1807bad95e4e24db28cc005f5ef25362f
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-11 14:27:24 +0100

    Add grid size changing

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector(".container");
 const colorPicker = document.querySelector(".colorPicker");
 const clearBtn = document.querySelector(".clearBtn");
-const gridSize = document.querySelector("output").value;
+const gridSizeDisplay = document.querySelector("output").value;
+const gridSizeInput = document.querySelector("#sizeInput");
 let size = 16;
 
 
@@ -9,6 +10,8 @@ let size = 16;
 
 
 function createGrid(size) {
+    container.textContent = '';
+
     for (let i = 0; i < size; i++) {
         const div = document.createElement("div");
         div.classList.add("row");
@@ -19,42 +22,58 @@ function createGrid(size) {
             column.classList.add("column");
             div.appendChild(column);
         }
+
     }
+
+    enableColoring();
 }
 
-createGrid(size);
-
-//track mouse state
-let isDrawing = false;
-
-//Add event listeners to column
-const columns = document.querySelectorAll(".column");
 
 
-columns.forEach((cell) => {
-    //start drawing when mousedown 
-    cell.addEventListener("mousedown", () => {
-        isDrawing = true;
-        cell.style["background-color"] = colorPicker.value;
-    })
+gridSizeInput.addEventListener('input', (e) => {
+    const gridSize = e.target.value;
 
-    //stop drawing
-    cell.addEventListener("mouseup", () => {
-        isDrawing = false;
-    })
-
-    //click and drag to color
-    cell.addEventListener("mouseover", () => {
-        if (isDrawing) {
-            cell.style["background-color"] = colorPicker.value;
-        }
-    })
+    size = gridSize;
+    createGrid(size);
 })
+
+
+
+function enableColoring() {
+    //track mouse state
+    let isDrawing = false;
+
+    //Add event listeners to column
+    const columns = document.querySelectorAll(".column");
+
+    columns.forEach((cell) => {
+        //start drawing when mousedown 
+        cell.addEventListener("mousedown", () => {
+            isDrawing = true;
+            cell.style["background-color"] = colorPicker.value;
+        })
+
+        //stop drawing
+        cell.addEventListener("mouseup", () => {
+            isDrawing = false;
+        })
+
+        //click and drag to color
+        cell.addEventListener("mouseover", () => {
+            if (isDrawing) {
+                cell.style["background-color"] = colorPicker.value;
+            }
+        })
+    })
+}
 
 //clear the grid
 clearBtn.addEventListener("click", () => {
+
+    const columns = document.querySelectorAll(".column");
     columns.forEach(cell => {
         cell.style["background-color"] = "white";
     })
 })
 
+createGrid(size);
